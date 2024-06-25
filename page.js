96cefc7983be2b40c490251cd7cffae2f02c2cb5
ArthurZane <129330715+ArthurZane@users.noreply.github.com>
2024-06-25 00:52:05 -0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,27 @@
+export default function home() {
+  return (
+  <div classname="container-fluid">
+    <div classname="row text-center">
+      <div classname="col-md-12 bg-danger p-3">
+        Navbar
+      </div>
+    </div>
+    <div classname="row text-center">
+      <div classname="col-md-12 bg-primary p-5">
+        Header
+      </div>
+    </div>
+    <div classname="row text-center">
+      <div classname="col-md-4 bg-success p-5">
+        card1
+      </div>
+      <div classname="col-md-4 bg-success p-5">
+        card2
+      </div>
+      <div classname="col-md-4 bg-success p-5">
+        card3
+      </div>
+    </div>
+  </div>
+  );
+  }
